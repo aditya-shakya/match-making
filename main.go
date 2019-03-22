eb6f673fd5d58e1afbf791f2c86a9d4d47589981
Aditya Shakya <aditya.shakya@nestaway.com>
2019-03-22 11:23:21 +0530

    adding struct for gametype

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	match "match-making/match"
 )
 
+type GameType = match.GameType
+
 func main() {
-	fmt.Println("hey")
+	fmt.Println("Starting Game simulation")
+	g := GameType{Name: "1v1", PlayerPot: 2}
+	fmt.Println(g)
+
 }
